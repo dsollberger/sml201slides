ebfad753baefb13424027639f57bc961226bce4b
Derek Sollberger <dsollberger@ucmerced.edu>
2026-01-18 19:20:22 -0500

    session 1
    session 1

--- posts/03_variance/scratch.r
+++
@@ -1,6 +0,0 @@
-library("yfR")
-today <- Sys.Date()
-start_date <- "2024-01-01"
-stock_market_example <- yf_collection_get(collection = "SP500", 
-                                          first_date = start_date, 
-                                          last_date = today)
